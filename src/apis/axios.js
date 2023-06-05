267a0f43c5bed7dd0507b7dde55ba29f200151f0
thaihung24 <hn58013@gmail.com>
2023-06-05 17:21:52 +0700

    17:21

--- src/apis/axios.js
+++ src/apis/axios.js
@@ -1,2 +1,2 @@
-// export const URL = 'https://ecom-kltn-k19-be.onrender.com/api'
-export const URL = 'http://localhost:5000/api'
+export const URL = 'https://ecom-kltn-k19-be.onrender.com/api'
+// export const URL = 'http://localhost:5000/api'
